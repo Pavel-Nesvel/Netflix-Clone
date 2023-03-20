8838474f819bfe4b2a74804b195f483369503d46
Pavel <pavelnesvel@yahoo.fr>
2023-03-20 14:13:53 +0100

    AuthProvider qui permet d'envelopper tous les composant danbs APP

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./Context/AuthContext";
 import { SignIn } from "./Auth/SignIn";
 import { SignUp } from "./Auth/SignUp";
 import { HomeCineme } from "./Pages/HomeCineme";
@@ -7,18 +8,18 @@ import { Tendances } from "./Pages/Tendances";
 
 function App() {
   return (
-    <BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomeCineme />} />
         <Route path="/myliste" element={<Mylist />} />
-        <Route path="/tendances" element={<Tendances/>} />
-        <Route path="/signIn" element={<SignIn/>} />
-        <Route path="/signUp" element={<SignUp/>} />
+        <Route path="/tendances" element={<Tendances />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
         <Route path="/*" element={<HomeCineme />} />
-      
       </Routes>
     </BrowserRouter>
-  
+    </AuthProvider>
   );
 }
 
