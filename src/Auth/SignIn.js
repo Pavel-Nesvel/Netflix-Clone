8cc2beca40e1d612a0f5cf06b0f3e8e4ff08838d
Pavel <pavelnesvel@yahoo.fr>
2023-03-20 17:48:15 +0100

    page de connexion avec firebase

--- src/Auth/SignIn.js
+++ src/Auth/SignIn.js
@@ -1,18 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../Context/AuthContext";
 import "../styles/signUp.css";
 
 export const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(true);
+  const { currentUser, login } = useAuth();
+  const navigate = useNavigate();
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    try {
+      setLoading(true);
+      await login(email, password);
+      navigate("/");
+    } catch (e) {
+      console.log("error",e)
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/");
+    }
+  }, [currentUser, navigate]);
+  console.log("value",email)
+  
     return (
         <div className="signUp-container">
           <div className="form-container">
             <h1>Connexion</h1>
-            <form >
-              <input type="text" name="email" id="email" placeholder="votre email" />
+            <form  onSubmit={handleSubmit} >
+              <input type="text" 
+              name="email"
+               id="email" 
+               value={email}
+               onChange={handleEmail}
+               placeholder="votre email" />
               <input
-                type="text"
+                type="passWord"
                 name="passWord"
                 id="passWord"
                 placeholder="mot de passe"
+                value={password}
+                onChange={handlePassword}
               />
               <input type="submit" value="Connexion" />
             </form>
