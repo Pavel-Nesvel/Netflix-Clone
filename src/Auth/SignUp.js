06be85d41907198e640bca74033737bc7a09a733
Pavel <pavelnesvel@yahoo.fr>
2023-03-20 15:57:32 +0100

    pade d'authentification avec firebase

--- src/Auth/SignUp.js
+++ src/Auth/SignUp.js
@@ -1,25 +1,90 @@
-import React from "react";
+import { async } from "@firebase/util";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../Context/AuthContext";
 import "../styles/signUp.css";
 
 export const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { currentUser, register } = useAuth();
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handlePasswordConfirm = (e) => {
+    setPasswordConfirm(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== passwordConfirm) {
+      return alert("les deux mot de passe ne correspondent pas !");
+    }
+    try {
+      setLoading(true);
+      await register(email, password);
+      navigate("/");
+    } catch (e) {
+      console.log("error",e)
+    }
+    setLoading(false);
+  };
+  useEffect(() => {
+    if (currentUser) {
+      navigate("/");
+    }
+  }, [currentUser, navigate]);
+
   return (
     <div className="signUp-container">
       <div className="form-container">
         <h1>S'inscrire</h1>
-        <form >
-          <input type="text" name="email" id="email" placeholder="votre email" />
+        <form onSubmit={handleSubmit}>
           <input
             type="text"
+            required
+            name="email"
+            id="email"
+            value={email}
+            onChange={handleEmail}
+            placeholder="votre email"
+          />
+          <input
+            type="passWord"
             name="passWord"
             id="passWord"
-            placeholder="mot de passe"
+            required
+            value={password}
+            onChange={handlePassword}
+            placeholder="votre mot de passe"
           />
-          <input type="submit" value="inscrption" />
+          <input
+            type="passWord"
+            name="passWordConfirm"
+            id="passWordConfirm"
+            required
+            value={passwordConfirm}
+            onChange={handlePasswordConfirm}
+            placeholder="confirmez votre mot de passe"
+          />
+          <input
+            type="submit"
+            value="inscrption"
+          />
         </form>
         <div className="signUp-count">
-            <h2>Vous etes déjà inscrit ?</h2>
-            <span> <NavLink to="/signIn">Cliquez ici !</NavLink>  </span>
+          <h2>Vous etes déjà inscrit ?</h2>
+          <span>
+            {" "}
+            <NavLink to="/signIn">Cliquez ici !</NavLink>{" "}
+          </span>
         </div>
       </div>
     </div>
