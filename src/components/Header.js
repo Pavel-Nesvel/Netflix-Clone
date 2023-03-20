3d5a3848b9d7d16b237ac8a7f51ed1b82bef90ee
Pavel <pavelnesvel@yahoo.fr>
2023-03-20 20:47:09 +0100

    ajout de la logique qui permet a un utilisateur de se deconnecter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
+import { async } from "@firebase/util";
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useAuth } from "../Context/AuthContext";
 import "../styles/Header.css";
 const logo = "/Netflix-Logo.wine.svg";
 export const Header = () => {
+  const { currentUser, logout } = useAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/signUp");
+    } catch (err) {
+      return err;
+    }
+  };
   return (
     <div className="navigation-header">
       <div className="header-logo">
@@ -14,25 +26,24 @@ export const Header = () => {
           <NavLink to="/tendances">Tendances</NavLink>
           <NavLink to="/">Home-Movies</NavLink>
           <NavLink to="/myliste">MyListe</NavLink>
-          <NavLink>
-            {
+          {currentUser?.email && (
+            <NavLink onClick={handleLogout}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
-                class="w-6 h-6"
-                className="logout"
+                className="w-6 h-6 logout"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
                 />
               </svg>
-            }
-          </NavLink>
+            </NavLink>
+          )}
         </li>
       </ul>
     </div>
