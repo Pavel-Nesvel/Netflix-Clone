1fc1d6a2625c28a9f13cc3416d08f37edca4e2f6
Pavel <pavelnesvel@yahoo.fr>
2023-03-20 17:46:58 +0100

    mise en place de l'authcontext, qui nous permet de pouvoir authentifier un utilisateur, l'utilisateur, l'utilisateur courent peut acceder nimporte ou sur l'application

---
+++ src/Context/AuthContext.js
@@ -0,0 +1,54 @@
+// AuthContext.js
+
+import { createContext, useContext, useState, useEffect } from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { signOut } from "firebase/auth";
+import auth from "../Auth/Config/Firebase";
+
+const AuthContext = createContext();
+
+//permet d'utiliser le context dans les autres composant
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  function register(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+  function logout() {
+    return signOut(auth);
+}
+
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+    login,
+    register,
+    logout,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
