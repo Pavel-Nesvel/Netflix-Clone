78e7d86cb44eafff520c10f3262d6563844851d4
Pavel <pavelnesvel@yahoo.fr>
2023-05-18 03:57:56 +0200

    fix:playliste des films de l'utilisateur

--- src/Pages/Mylist.js
+++ src/Pages/Mylist.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "../components/Header";
+import { getMovieId } from "../API/SearchMovie";
+import { MylisteCard } from "../components/MylisteCard";
 
 export const Mylist = () => {
-  console.log("tendance");
+  const [listeData, setListeData] = useState([]);
+
+  useEffect(() => {
+    let getId = async () => {
+      let listeMovieId = window.localStorage.movies
+        ? window.localStorage.movies.split(",")
+        : [];
+      for (let i = 0; i < listeMovieId.length; i++) {
+        let id = await getMovieId(listeMovieId[i]);
+        setListeData((listeData) => [...listeData, id.data]);
+      }
+    };
+    getId();
+  }, []);
   return (
-    <div>
+    <div className="user-list-page">
       <Header />
 
-      <h1>Ma liste de film</h1>
-      <h1>Ma liste de film</h1>
+      <h2 className=" user-heart">
+        Coup de coeur <span>❤️</span>
+      </h2>
+
+      <div className="user-container">
+        {listeData.length > 0 ? (
+          listeData.map((movie, key) => <MylisteCard movie={movie} key={key} />)
+        ) : (
+          <h2>Votre liste ne contient aucun film pour le moment</h2>
+        )}
+      </div>
     </div>
   );
 };
